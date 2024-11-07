3320a91190b4bf027a6728e6ef5cdbc2f4d4ade8
dutra1985 <cleberdutra@prof.educacao.sp.gov.br>
2024-11-07 17:09:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que são HTML e CSS?',
+    'O HTML e CSS são linguagens de programação usadas para criar páginas web.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austrália?',
+    'A capital da Austrália é Sidney'
+)
+
+criaCartao(
+    'História',
+    'Qual era a função das Pirâmides?',
+    'Era para ser as tumbas dos Faraós'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Bom Dia em Inglês?',
+    'Bom Dia em ingles é Good Morning'
+)
+
+criaCartao(
+    'Educação Física',
+    'Quantos jogadores existem em quadra numa partidade de Handebol?',
+    'Serão 14 jogadores, sete de cada lado.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o principal gás intensificador do efeito estufa?',
+    'Gás Carbonico (CO2).'
+)
